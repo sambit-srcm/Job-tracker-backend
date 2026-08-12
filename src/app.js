b69f1d2c9550efd1ea15b0c80c4c7c51b85df7e8
se7ensam <sambitmohanty342@gmail.com>
2026-08-13 00:40:51 +0530

    chore: Add initial project structure

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+module.exports = app;
