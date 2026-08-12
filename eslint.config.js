b90be9e533254805ad24c181e9a91a9ef2e9a7cd
se7ensam <sambitmohanty342@gmail.com>
2026-08-13 00:52:15 +0530

    chore: Add dev tooling and CI setup

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
+  eslintConfigPrettier,
+  {
+    ignores: ['node_modules/', 'coverage/'],
+  },
+];
